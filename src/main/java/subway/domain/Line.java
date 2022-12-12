aa9c5f62273f61718ded5a4beb07b606f5d2f697
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-12-12 23:22:29 +0900

    feat: Line 객체가 관리하는 역 정보의 추가, 삭제를 구현한 기능 추가
    추가 설명:
    - 구간 등록 메서드 addStationByOrder: 순서를 정해 노선에 역을 등록하고자 할때 사용.
    - 구간 삭제 메서드 deleteGivenStation: 노선에 존재하는 임의의 역을 삭제하고자 할때 사용.
    - 구간 삭제 성공 시 true, 실패 시 false 반환.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -23,6 +23,47 @@ public class Line {
         this.existingStations.add(station);
     }
 
+    // 구간 등록: 역이 등록될 순서를 정하는 메서드
+    public void addStationByOrder(Station newStation, int order) {
+        List<Station> temp = List.copyOf(existingStations);
+        existingStations.clear();
+        for (int i = 0; i < order - 1; i++) {
+            existingStations.add(temp.get(i));
+        }
+        existingStations.add(newStation);
+        for (int i = order - 1; i < temp.size(); i++) {
+            existingStations.add(temp.get(i));
+        }
+    }
+
+    // 구간 삭제: 임의의 역을 삭제하는 메서드
+    public boolean deleteGivenStation(String deleteStation) {
+        int order = getIndexOfStation(deleteStation) + 1;
+        if (order == 0) {
+            return false;
+        }
+        List<Station> temp = List.copyOf(existingStations);
+        existingStations.clear();
+
+        for (int i = 0; i < order - 1; i++) {
+            existingStations.add(temp.get(i));
+        }
+        for (int i = order; i < temp.size(); i++) {
+            existingStations.add(temp.get(i));
+        }
+        return true;
+    }
+
+    // 역이 위치하는 인덱스를 반환하는 메서드
+    private int getIndexOfStation(String name) {
+        for (int i = 0; i < existingStations.size(); i++) {
+            if (existingStations.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public List<Station> getExistingStations() {
         return existingStations;
     }
